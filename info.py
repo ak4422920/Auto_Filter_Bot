3e82866b440a395a6e4385a1b171f08f51c5346f
ak4422920 <ak4422920@gmail.com>
2026-02-21 11:37:46 +0530

    Update info.py

--- info.py
+++ info.py
@@ -188,35 +188,34 @@ BAD_WORDS = {
    
 
 # ============================
-# Server & Web Configuration
+# Server & Web Configuration (Fixed by ZeroDay)
 # ============================
+ON_HEROKU = 'DYNO' in environ
+APP_NAME = environ.get('APP_NAME')
 
-NO_PORT = bool(environ.get('NO_PORT', False))
-APP_NAME = None
-if 'DYNO' in environ:
-    ON_HEROKU = True
-    APP_NAME = environ.get('APP_NAME')
-else:
-    ON_HEROKU = False
+# Agar FQDN env mein nahi hai, toh default tera Koyeb URL use hoga
 BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', '0.0.0.0'))
-FQDN = str(getenv('FQDN', BIND_ADRESS)) if not ON_HEROKU or getenv('FQDN') else APP_NAME+'.herokuapp.com'
-URL = "https://{}/".format(FQDN) if ON_HEROKU or NO_PORT else "https://{}/".format(FQDN, PORT)
+FQDN = str(getenv('FQDN', 'xenophobic-francisca-akmoviebotz-019ea95c.koyeb.app'))
+
+# Heroku ke liye dynamic FQDN setup
+if ON_HEROKU and APP_NAME:
+    FQDN = f"{APP_NAME}.herokuapp.com"
+
+# SSL aur URL Logic
+HAS_SSL = is_enabled(getenv('HAS_SSL', "True"), True)
+if HAS_SSL:
+    URL = "https://{}/".format(FQDN)
+else:
+    URL = "http://{}/".format(FQDN)
+
+NO_PORT = is_enabled(environ.get('NO_PORT', "False"), False)
+PORT = environ.get("PORT", "8080")
 SLEEP_THRESHOLD = int(environ.get('SLEEP_THRESHOLD', '60'))
 WORKERS = int(environ.get('WORKERS', '4'))
 SESSION_NAME = str(environ.get('SESSION_NAME', 'dreamXBotz'))
 MULTI_CLIENT = False
 name = str(environ.get('name', 'DREAMXBOTZ'))
-PING_INTERVAL = int(environ.get("PING_INTERVAL", "1200"))  # 20 minutes
-if 'DYNO' in environ:
-    ON_HEROKU = True
-    APP_NAME = str(getenv('APP_NAME'))
-else:
-    ON_HEROKU = False
-HAS_SSL = bool(getenv('HAS_SSL', True))
-if HAS_SSL:
-    URL = "https://{}/".format(FQDN)
-else:
-    URL = "http://{}/".format(FQDN)
+PING_INTERVAL = int(environ.get("PING_INTERVAL", "1200")) # 20 minutes
 
 # ============================
 # Reactions Configuration
